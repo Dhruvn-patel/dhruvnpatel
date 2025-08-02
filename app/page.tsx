3e9b855a276141991f67e29d9f8279a593b2e13a
Dhruvn-patel <dhruvpateldhruvpatel01669@gmail.com>
2025-08-02 16:17:04 +0530

    fixes bugs

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Hero from "@/components/hero";
 import About from "@/components/about";
 import TechStack from "@/components/tech-stack";
 import Projects from "@/components/projects";
-import Experience from "@/components/Experience";
+import Experience from "@/components/experience";
 import Testimonials from "@/components/testimonials";
 import Contact from "@/components/contact";
 import Footer from "@/components/footer";
